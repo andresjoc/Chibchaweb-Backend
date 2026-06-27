d95f929933bcbd6f4da9d5009f2d5a592f73162c
andresjoc <andresjocgit@gmail.com>
2026-06-27 02:00:28 -0500

    Details

--- api/security.py
+++ api/security.py
@@ -1,9 +1,11 @@
-from fastapi import Header, HTTPException
+from fastapi import Header, HTTPException, Request
 import os
 
 API_KEY = os.getenv("API_KEY")
 API_KEY_NAME = os.getenv("API_KEY_NAME", "Chibcha-api-key")
 
-def verificar_api_key(x_api_key: str = Header(..., alias=API_KEY_NAME)):
-    if x_api_key != API_KEY:
+def verificar_api_key(request: Request, x_api_key: str = Header(None, alias=API_KEY_NAME)):
+    if request.method == "OPTIONS":
+        return
+    if not x_api_key or x_api_key != API_KEY:
         raise HTTPException(status_code=403, detail="API Key inválida")
